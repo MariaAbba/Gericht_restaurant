1368bb926322569c21cfe6ce5b1482d5c7ac2ec5
Mary <gretchen7887@gmail.com>
2024-02-10 19:14:46 +0400

    Upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
-function App() {
-  return <>Meow</>
-}
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from './container'
+import { Navbar } from './components'
+
+const App = () => (
+    <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+)
 
 export default App
