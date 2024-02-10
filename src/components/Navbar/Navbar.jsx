e2a1569f98fb424f6efc8cde7d011eef1123e996
Mary <gretchen7887@gmail.com>
2024-02-10 22:20:00 +0400

    Upd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {MdOutlineRestaurantMenu} from 'react-icons/md'
+import React from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
 
 import images from '../../constants/images'
 
-import './Navbar.css';
-
+import './Navbar.css'
 
 const Navbar = () => (
-  <nav className='app__navbar'>
-    <div className="app__navbar__logo">
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
       <img src={images.gericht} alt="app logo" />
     </div>
     <ul className="app__navbar-links">
-      <li className="p__opensans"><a href="#home">Home</a></li>
-      <li className="p__opensans"><a href="#about">About</a></li>
-      <li className="p__opensans"><a href="#menu">Menu</a></li>
-      <li className="p__opensans"><a href="#awards">Awards</a></li>
-      <li className="p__opensans"><a href="#contacts">Contacts</a></li>
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contacts">Contacts</a>
+      </li>
     </ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Log In / Register
+      </a>
+      <div />
+      <a href="/" className="p__opensans">
+        Book Table
+      </a>
+    </div>
   </nav>
-);
+)
 
-export default Navbar;
+export default Navbar
