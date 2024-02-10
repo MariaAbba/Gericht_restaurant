57a55efce72cd4189510a4b58ebade84b353db15
Mary <gretchen7887@gmail.com>
2024-02-10 20:40:47 +0400

    Upd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {MdOutlineRestaurantMenu} from 'react-icons/md'
+
+import images from '../../constants/images'
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
+  <nav className='app_navbar'>
     Navbar
-  </div>
+  </nav>
 );
 
 export default Navbar;
