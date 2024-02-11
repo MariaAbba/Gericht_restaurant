c04d2abc205b3751b4a4c09a7a408ed62a3ab38a
Mary <gretchen7887@gmail.com>
2024-02-11 13:24:21 +0400

    Upd

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -6,8 +6,8 @@ const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem' }}>
     <p className="p__cormorant">
       {title}
-      <img src={images.spoon} alt="spoon" className="spoon__img" />
     </p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 )
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,8 +9,17 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Embark on a culinary journey with us, where innovation meets elegance,
+        and each bite tells a story of exquisite flavors.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 )
 
