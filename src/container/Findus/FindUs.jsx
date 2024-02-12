0d1b9059d444f7f116e55d732e2599678671c4dd
Mary <gretchen7887@gmail.com>
2024-02-12 18:06:27 +0400

    Upd

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -14,16 +14,18 @@ const FindUs = () => (
         <p className="p__opensans">
           Lane Ends Bugalow, Whatcroft Hall Lane, Rudheath, CW9 75G
         </p>
-        <p className="p__cormorant" style={{color:'DCCA87', margin: '2rem 0'}}>
+        <p
+          className="p__cormorant"
+          style={{ color: 'DCCA87', margin: '2rem 0' }}
+        >
           Opening Hours
         </p>
-        <p className="p__opensans">
-         Mon -Fri: 10: 00 am - 02:00 am
-        </p>
-        <p className="p__opensans">
-         Sat -Sun: 10: 00 am - 03:00 am
-        </p>
+        <p className="p__opensans">Mon -Fri: 10: 00 am - 02:00 am</p>
+        <p className="p__opensans">Sat -Sun: 10: 00 am - 03:00 am</p>
       </div>
+      <button className="custom__button" style={{ marginTop: '2rem' }}>
+        Visit Us
+      </button>
     </div>
     <div className="app__wrapper_img">
       <img src={images.findus} alt="findus" />
