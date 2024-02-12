a576bc88dbb1e7ad0aa8b68bdd44bd6e908fd283
Mary <gretchen7887@gmail.com>
2024-02-12 11:54:54 +0400

    Upd

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -5,6 +5,38 @@ import { SubHeading } from '../../components'
 
 import './Chef.css'
 
-const Chef = () => <div>Chef</div>
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content-quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            With a profound commitment to excellence, we craft dishes that tell
+            stories of passion, innovation, and a love for exceptional flavors.
+          </p>
+        </div>
+          <p className="p__opensans">
+            Every plate is a canvas, and every bite is an exploration of taste
+            designed to captivate your senses and elevate your dining
+            experience. Welcome to a world where the chef′s vision comes to life
+            on your plate, a symphony of ingredients harmonizing to create
+            unforgettable moments.
+          </p>
+
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
+      </div>
+    </div>
+  </div>
+)
 
 export default Chef
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import { images } from '../../constants'
+import React, { useState } from 'react'
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../../constants'
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import './Intro.css'
 
-export default Intro;
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false)
+  const vidRef = React.useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if (playVideo) {
+      vidRef.current.pause()
+          } else {
+      vidRef.current.play()
+          }
+  }
+
+  return (
+    <div>
+      <div className="app__video">
+        <video
+          src={meal}
+          ref={vidRef}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+        />
+        <div className="app__video-overlay flex__center">
+          <div
+            className="app__video-overlay_circle flex__center"
+            onClick={handleVideo}
+          >
+            {playVideo ? (
+              <BsPauseFill color="#fff" fontSize={38} />
+            ) : (
+              <BsFillPlayFill color="#fff" fontSize={38} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
