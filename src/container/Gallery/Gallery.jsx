6d5ed22e8ecc58e4dc781654923afb3b925948fd
Mary <gretchen7887@gmail.com>
2024-02-12 16:04:01 +0400

    Upd

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -11,7 +11,25 @@ import { images } from '../../constants'
 
 import './Gallery.css'
 
+const galleryImages = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+]
 const Gallery = () => {
+  const scrollRef = React.useRef(null)
+
+  const scroll = (direction) => {
+    const { current } = scrollRef
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300
+    } else {
+      current.scrollRight += 300
+    }
+  }
+
   return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
@@ -24,6 +42,28 @@ const Gallery = () => {
           us on a virtual journey through the lens, and let the images whet your
           appetite for an unforgettable dining experience. Bon appétit!
         </p>
+        <button className="custom__button">View More</button>
+      </div>
+      
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
+              <img src={image} alt='gallery'/>
+              <BsInstagram className='gallery__image-icon'/>
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('right')}
+          />
+        </div>
       </div>
     </div>
   )
