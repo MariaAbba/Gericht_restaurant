c81c3fcfa58bfe2885c419c9bcf75c45c559fa79
Mary <gretchen7887@gmail.com>
2024-02-12 18:26:56 +0400

    Upd

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import SubHeading from '../SubHeading/SubHeading'
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className="app__newsletter-heading">
+      <SubHeading title='Newsletter'/>
+      <h1 className='headtext__cormorant'></h1>
+    </div>
   </div>
 );
 
